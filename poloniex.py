47aa6b88d878d065976ec26a3c2dd2dc54dd7e88
Tazeek <tazeek@binary.com>
2017-05-30 12:51:04 +0800

    Time stamp function created

--- poloniex.py
+++ poloniex.py
@@ -1,6 +1,11 @@
-import urllib
-import urllib2
+from urllib.request import urlopen
+
 import json
 import time
 import hmac
 import hashlib
+
+# Function for creating timestamp
+def createTimeStamp(datestr, format="%Y-%m-%d %H:%M:%S"):
+
+	return time.mktime(time.strptime(datestr, format))
