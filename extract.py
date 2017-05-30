af7c3919e17d5a300e45844cd52243e0fe79fc68
Tazeek <tazeek@binary.com>
2017-05-30 15:51:29 +0800

    Aggregation added

--- extract.py
+++ extract.py
@@ -1,6 +1,6 @@
 from poloniex import Poloniex 
 
-import pandas as pd 
+import pandas as pd
 
 # Account key 
 APIKEY = "PUT APIKEY HERE"
@@ -9,7 +9,7 @@ SECRET = "PUT SECRET HERE"
 # Create poloniex object and initialize currency pair
 polo = Poloniex(APIKEY, SECRET)
 currency_pair = 'USDT_BTC'
-depth = 50
+depth = 20000
 
 # Get the data
 pair_data = polo.returnOrderBook(currency_pair, depth)
@@ -20,9 +20,17 @@ bids_array = pair_data['bids']
 # Create dataframe from 'asks' data
 asks_df = pd.DataFrame(asks_array, columns=['askRate', 'askAmount'])
 asks_df['askRate'] = asks_df['askRate'].apply(pd.to_numeric)
+asks_df['Product'] = asks_df['askRate'] * asks_df['askAmount']
+
+# Create dataframe from 'bids' data
+bids_df = pd.DataFrame(bids_array, columns = ['bidRate', 'bidAmount'])
+bids_df['bidRate'] = bids_df['bidRate'].apply(pd.to_numeric)
+bids_df['Product'] = bids_df['bidRate'] * bids_df['bidAmount']
+
+# Aggregation of 'Rate' column
+total_ask_product = asks_df['Product'].sum() 
+total_bid_product = bids_df['Product'].sum()
 
 # Test Print
-print("\n\n", asks_array, "\n\n")
-print(bids_array, "\n\n")
-print(asks_df.head(10), "\n\n")
-print(asks_df.dtypes)
+print(total_ask_product,"\n")
+print(total_bid_product, "\n")
